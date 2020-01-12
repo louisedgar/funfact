0870b75f472abcc2895a929761b27080d5be9ce7
louisedgar <louisedgar@ymail.com>
2020-01-12 16:41:30 +0700

    first

---
+++ script.js
@@ -0,0 +1,16 @@
+// fetch("fakta-fakta.txt")
+//   .then(response => {
+//     return response.text();
+//   })
+//   .then(data => {
+//     let facts = "<div>Fakta<div>";
+//     data.filter(data => {
+//       let id = Math.round(Math.random() * 50);
+//       if (data === id) {
+//         facts += `<p>${data.id}<p>`;
+//       }
+//     });
+//     document.getElementById("json").innerHTML = facts;
+//   });
+
+// http://5e19bc3ccc623b00146789fc.mockapi.io/funfact/facts
